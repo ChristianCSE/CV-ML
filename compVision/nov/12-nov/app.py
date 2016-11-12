0dbd0a72618bfc3da40b02761e1e7202f9fd1252
ChristianCSE <csanch4@emory.edu>
2016-11-12 12:54:34 -0800

    blobbing

--- compVision/nov/12-nov/app.py
+++ compVision/nov/12-nov/app.py
@@ -1,9 +1,29 @@
-import cv
-import sys
+# import cv
+# import sys
 from SimpleCV import Camera, Display, Image
+import time
+# #init the camera
+# cam = Camera(0)
+
+# # cv.GrabFrame(cam.capture) checks
+# # frame = cv.RetrieveFrame(cam.capture) checks
+
+# #init display
+# display = Display()
+
+# #snap pic using camera
+# img = cam.getImage()
+
+# #show pic on screen
+# img.save(display)
+
+
+
+###improved hello world
 cam = Camera(0)
-# cv.GrabFrame(cam.capture)
-# frame = cv.RetrieveFrame(cam.capture)
 display = Display()
 img = cam.getImage()
 img.save(display)
+img.drawText("Hello Hello Hello Hello Hello Hello World!")
+img.save(display)
+time.sleep(5)
